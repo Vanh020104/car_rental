49c44b53734e3bfd4c481c5a746834f235e290c8
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2023-10-17 14:00:47 +0700

    Auth

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/delete-from-cart/{product}', [\App\Http\Controllers\HomeController:
 Route::get('/clear-cart', [\App\Http\Controllers\HomeController::class, "clearCart"]);
 Route::get('/checkout/{slug}', [\App\Http\Controllers\HomeController::class, "checkoutSlug"])->name('checkoutSlug');
 Route::post('/bill/{product}', [\App\Http\Controllers\HomeController::class, 'billCart']);
-Route::post('/checkout', [\App\Http\Controllers\HomeController::class, 'processCheckout'])->name('checkout.process');
+Route::post('/checkout/process', [\App\Http\Controllers\HomeController::class, 'processCheckout'])->name('checkout.process');
 Route::get('/checkout/{order}', [\App\Http\Controllers\HomeController::class, 'checkoutForm'])->name('checkout.form');
 
 
@@ -30,3 +30,7 @@ Route::get('/checkout/{order}', [\App\Http\Controllers\HomeController::class, 'c
 Route::post('/checkout', [\App\Http\Controllers\HomeController::class,"placeOrder"]);
 Route::get('/paypal-success/{order}', [\App\Http\Controllers\HomeController::class,"paypalSuccess"]);
 Route::get('/paypal-cancel/{order}', [\App\Http\Controllers\HomeController::class,"paypalCancel"]);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
